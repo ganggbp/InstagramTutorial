b1e5d7e2448651ba6e8a712adfff088157b10ef8
Burit Boonkorn <burit_bk@hotmail.com>
2024-01-10 00:52:14 +0700

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Burit Boonkorn on 10/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
