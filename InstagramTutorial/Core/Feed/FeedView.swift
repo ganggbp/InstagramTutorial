79c8599706c9eec4bc70c440c639db782a29b73d
Burit Boonkorn <burit_bk@hotmail.com>
2024-01-13 20:41:59 +0700

    finish feed view ui

---
+++ InstagramTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,41 @@
+//
+//  FeedView.swift
+//  InstagramTutorial
+//
+//  Created by Burit Boonkorn on 13/1/2567 BE.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("instagram-logo-black")
+                        .resizable()
+                        .frame(width: 100, height: 32)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
