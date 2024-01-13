8fe5df2d7a7838aaebfe2ade5ccdb8c3ca680e98
Burit Boonkorn <burit_bk@hotmail.com>
2024-01-13 20:49:37 +0700

    finish search view ui

---
+++ InstagramTutorial/Core/Search/SearchView.swift
@@ -0,0 +1,49 @@
+//
+//  SearchView.swift
+//  InstagramTutorial
+//
+//  Created by Burit Boonkorn on 13/1/2567 BE.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0 ... 15, id: \.self) { user in
+                        HStack {
+                            Image("batman-2")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("batman")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Bruce Weyne")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
